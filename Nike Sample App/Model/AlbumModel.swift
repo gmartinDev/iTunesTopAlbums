7f60814f5f6f0246c5b18d278a07193b4b383a5e
gmartin <gregm6107@gmail.com>
2020-08-18 11:38:00 -0500

    Decoding using .decodeIfPresent
    This is to handle json not containing all the keys in the models - will stay as a nil value

--- Nike Sample App/Model/AlbumModel.swift
+++ Nike Sample App/Model/AlbumModel.swift
@@ -31,31 +31,21 @@ struct AlbumModel: Decodable {
         case url
     }
     
-    init() {
-        artistName = nil
-        id = nil
-        releaseDateString = nil
-        name = nil
-        kind = nil
-        copyright = nil
-        artworkUrl = nil
-        genres = nil
-        url = nil
-    }
+    //Used in tests to initialize the expected result
+    init() { }
     
     init(from decoder: Decoder) throws {
         do {
             let container = try decoder.container(keyedBy: AlbumKeys.self)
-            
-            artistName = try container.decode(String.self, forKey: .artistName)
-            id = try container.decode(String.self, forKey: .id)
-            releaseDateString = try container.decode(String.self, forKey: .releaseDateString)
-            name = try container.decode(String.self, forKey: .name)
-            kind = try container.decode(String.self, forKey: .kind)
-            copyright = try container.decode(String.self, forKey: .copyright)
-            artworkUrl = try container.decode(URL.self, forKey: .artworkUrl)
-            genres = try container.decode([GenreModel].self, forKey: .genres)
-            url = try container.decode(URL.self, forKey: .url)
+            artistName = try container.decodeIfPresent(String.self, forKey: .artistName)
+            id = try container.decodeIfPresent(String.self, forKey: .id)
+            releaseDateString = try container.decodeIfPresent(String.self, forKey: .releaseDateString)
+            name = try container.decodeIfPresent(String.self, forKey: .name)
+            kind = try container.decodeIfPresent(String.self, forKey: .kind)
+            copyright = try container.decodeIfPresent(String.self, forKey: .copyright)
+            artworkUrl = try container.decodeIfPresent(URL.self, forKey: .artworkUrl)
+            genres = try container.decodeIfPresent([GenreModel].self, forKey: .genres)
+            url = try container.decodeIfPresent(URL.self, forKey: .url)
         } catch (let error as NSError) {
             print(error.localizedDescription)
         }
